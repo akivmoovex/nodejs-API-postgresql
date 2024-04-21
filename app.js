dc1258942d5d1e89224286b9fb372a57af459c19
Akivs <akivmoovex@gmail.com>
2024-04-21 19:55:49 +0300

    layouts

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const db = require('./models/index.js');
+
 const express = require('express');
 const app = express();
 const path = require('path');
@@ -8,9 +10,21 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.urlencoded());
 
+/*
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
+*/
+
+app.get('/', function(req, res) {
+    db.Url.findAll({order: [['createdAt', 'DESC']], limit: 5})
+    .then(urlObjs => {
+      res.render('index', {
+        urlObjs: urlObjs
+      });
+    });
+  });
+
 
 app.post('/url', function(req, res) {
   const url = req.body.url
@@ -21,3 +35,19 @@ app.post('/url', function(req, res) {
 });
 
 app.listen(port, () => console.log(`url-shortener listening on port ${port}!`));
+
+app.post('/url', function(req, res) {
+    const url = req.body.url
+  
+    urlShortener.short(url, function(err, shortUrl) {
+      db.Url.findOrCreate({where: {url: url, shortUrl: shortUrl}})
+      .then(([urlObj, created]) => {
+        res.send(shortUrl)
+      });
+    });
+  });
+
+const exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
